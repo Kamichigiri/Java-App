ef657e4ad29115ea5a6da36fc808e6a35805c949
Kamichigiri <115007308+Kamichigiri@users.noreply.github.com>
2024-06-01 15:59:42 +0100

    Add files via upload

---
+++ JavaOop/app/src/main/java/javaoop/Tools.java
@@ -0,0 +1,27 @@
+package javaoop;
+
+public class Tools {
+    private String category;
+    private String name;
+    private String inStock;
+    
+    public String getInStock() {
+        return inStock;
+    }
+    public void setInStock(String inStock) {
+        this.inStock = inStock;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
