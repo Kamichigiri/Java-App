18580cfc626f3dcaeca2c26d30ebbb601ef8d45a
Kamichigiri <115007308+Kamichigiri@users.noreply.github.com>
2024-06-26 09:10:32 +0100

    Add files via upload
    Test commit

---
+++ ussdtestv1/src/main/java/com/ussdtestv1/ussdtestv1/Ussdtestv1Application.java
@@ -0,0 +1,13 @@
+package com.ussdtestv1.ussdtestv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ussdtestv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ussdtestv1Application.class, args);
+	}
+
+}
---
+++ ussdtestv1/src/test/java/com/ussdtestv1/ussdtestv1/Ussdtestv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ussdtestv1.ussdtestv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ussdtestv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
